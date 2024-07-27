762737b5e76061c9237d888786787dee2d561acb
Piotr Ackermann <pioacker@gmail.com>
2024-07-27 11:53:07 +0200

    Red Box parsed

--- src/Gltf.hs
+++ src/Gltf.hs
@@ -16,6 +16,9 @@ type Index = Int
 data Gltf = Gltf {
     accessors :: Maybe [Accessor],
     asset :: Asset,
+    buffers :: Maybe [Buffer],
+    bufferViews :: Maybe[BufferView],
+    materials :: Maybe [Material],
     meshes :: Maybe [Mesh],
     nodes :: Maybe [Node],
     scene :: Maybe Index,
@@ -74,6 +77,55 @@ instance ToJSON Asset where
 instance FromJSON Asset where
     parseJSON = genericParseJSON readOptions
 
+data Buffer = Buffer {
+    byteLength :: Index,
+    name :: Maybe String,
+    uri :: Maybe String
+} deriving (Generic, Show)
+
+instance ToJSON Buffer where
+    toEncoding = genericToEncoding writeOptions
+
+instance FromJSON Buffer where
+    parseJSON = genericParseJSON readOptions
+
+data BufferView = BufferView {
+    buffer :: Index,
+    byteOffset :: Maybe Index,
+    byteLength :: Index,
+    byteStride :: Maybe Index,
+    name :: Maybe String,
+    target :: Maybe Index
+} deriving (Generic, Show)
+
+instance ToJSON BufferView where
+    toEncoding = genericToEncoding writeOptions
+
+instance FromJSON BufferView where
+    parseJSON = genericParseJSON readOptions
+
+data Material = Material {
+    name :: Maybe String,
+    pbrMetallicRoughness :: PbrMetallicRoughness
+} deriving (Generic, Show)
+
+instance ToJSON Material where
+    toEncoding = genericToEncoding writeOptions
+
+instance FromJSON Material where
+    parseJSON = genericParseJSON readOptions
+
+data PbrMetallicRoughness = PbrMetallicRoughness {
+    baseColorFactor :: Maybe [Number],
+    metallicFactor :: Maybe Number
+} deriving (Generic, Show)
+
+instance ToJSON PbrMetallicRoughness where
+    toEncoding = genericToEncoding writeOptions
+
+instance FromJSON PbrMetallicRoughness where
+    parseJSON = genericParseJSON readOptions
+
 data Mesh = Mesh {
     name :: Maybe String,
     primitives :: Maybe [Primitive]
@@ -83,19 +135,6 @@ instance ToJSON Mesh where
     toEncoding = genericToEncoding writeOptions
 
 instance FromJSON Mesh where
-    parseJSON = genericParseJSON readOptions
-
-data Node = Node {
-    children :: Maybe [Index],
-    matrix :: Maybe [Number],
-    mesh :: Maybe Index,
-    name :: Maybe String
-} deriving (Generic, Show)
-
-instance ToJSON Node where
-    toEncoding = genericToEncoding writeOptions
-
-instance FromJSON Node where
     parseJSON = genericParseJSON readOptions
 
 data Primitive = Primitive {
@@ -109,6 +148,19 @@ instance ToJSON Primitive where
     toEncoding = genericToEncoding writeOptions
 
 instance FromJSON Primitive where
+    parseJSON = genericParseJSON readOptions
+
+data Node = Node {
+    children :: Maybe [Index],
+    matrix :: Maybe [Number],
+    mesh :: Maybe Index,
+    name :: Maybe String
+} deriving (Generic, Show)
+
+instance ToJSON Node where
+    toEncoding = genericToEncoding writeOptions
+
+instance FromJSON Node where
     parseJSON = genericParseJSON readOptions
 
 data Scene = Scene {
