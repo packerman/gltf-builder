5c8d959b6c1fc8feb067a0d6e8eee142322d3a24
Piotr Ackermann <pioacker@gmail.com>
2024-08-06 00:17:58 +0200

    Decode buffer

--- src/Core/Decode.hs
+++ src/Core/Decode.hs
@@ -1,29 +1,43 @@
-{-# LANGUAGE DisambiguateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 
-module Core.Decode (decodeScene) where
+module Core.Decode (decodeScene, decodeBuffer, byteStringLength) where
 
 import Data.Vector (Vector, (!?))
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
 import Data.Maybe
+import qualified Data.Text as T
+import Data.Text.Encoding (encodeUtf8)
+import Data.ByteString.Base64 (decodeBase64)
 
 import Core.Model as Model
-import Gltf.Json (Gltf, GltfArray, Buffer, toVector, Index)
-import Util (maybeToEither)
+import Gltf.Json (Gltf, GltfArray, Buffer(..), toVector, Index)
+import Util (maybeToEither, mapLeft, validate)
 import qualified Gltf.Json as Gltf (Gltf(..), Node, Mesh(..))
 import qualified Gltf.Json as Scene (Scene(..))
 import Linear (V4 (V4), identity)
 
+byteStringLength = BS.length
+
 decodeScene :: Int -> Gltf -> Either String Scene
 decodeScene index gltf = do
     gltfScene <- getIndexed (Gltf.scenes gltf) index "scene"
-    let buffers = decodeBuffer <$> toVector (Gltf.buffers gltf)
-
+    buffers <- traverse decodeBuffer $ toVector (Gltf.buffers gltf)
     let nodes = decodeNode <$> toVector (Gltf.nodes gltf)
     scene (Scene.name gltfScene) <$> getByIndices nodes "node" (fromMaybe [] $ Scene.nodes gltfScene)
 
-decodeBuffer :: Buffer -> ByteString
-decodeBuffer = undefined
+decodeBuffer :: Buffer -> Either String ByteString
+decodeBuffer (Buffer { uri = maybeUri, byteLength }) =
+    case maybeUri of
+        Just uri -> mapLeft T.unpack $ decodeUri uri
+        Nothing -> error "No uri in buffer"
+    where
+        base64prefix = "data:application/octet-stream;base64,"
+        decodeUri uri | base64prefix `T.isPrefixOf` uri =
+                let dataPart = T.drop (T.length base64prefix) uri
+                in decodeBase64 $ encodeUtf8 dataPart
+            | otherwise = Left "Unsupported uri format"
 
 decodeNode :: Gltf.Node -> Node
 decodeNode _ = Model.defaultNode
--- src/Util.hs
+++ src/Util.hs
@@ -2,3 +2,14 @@ module Util (module Util) where
 
 maybeToEither :: a -> Maybe b -> Either a b
 maybeToEither def = maybe (Left def) Right
+
+mapLeft :: (a -> b) -> Either a c -> Either b c
+mapLeft f (Left x) = Left (f x)
+mapLeft _ (Right y) = Right y
+
+validateEither :: (a -> Bool) -> b -> Either b a -> Either b a
+validateEither _ _ e@(Left _) = e
+validateEither p d e@(Right x) = if p x then e else Left d
+
+validate :: (a -> Bool) -> b -> a -> Either b a
+validate p e x = if p x then Right x else Left e
