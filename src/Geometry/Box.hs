6856affa459f59387a2cefaa9b6e636d6540c07a
Piotr Ackermann <pioacker@gmail.com>
2024-10-27 17:58:35 +0100

    Box geometry

--- src/Geometry/Box.hs
+++ src/Geometry/Box.hs
@@ -6,11 +6,17 @@ import Linear
 
 box :: Float -> Float -> Float -> Geometry
 box a b c =
-  concatGeometries
-    [ tiledPlane (V3 (-(a / 2)) (-(b / 2)) (c / 2)) (V3 a 0 0) (V3 0 b 0) 1 1,
-      tiledPlane (V3 (a / 2) (-(b / 2)) (c / 2)) (V3 0 0 (-a)) (V3 0 b 0) 1 1,
-      tiledPlane (V3 (a / 2) (-(b / 2)) (-(c / 2))) (V3 (-a) 0 0) (V3 0 b 0) 1 1,
-      tiledPlane (V3 (-(a / 2)) (-(b / 2)) (-(c / 2))) (V3 0 0 a) (V3 0 b 0) 1 1,
-      tiledPlane (V3 (a / 2) (b / 2) (-(c / 2))) (V3 a 0 0) (V3 0 0 (-c)) 1 1,
-      tiledPlane (V3 (a / 2) (-(b / 2)) (-(c / 2))) (V3 (-a) 0 0) (V3 0 b 0) 1 1
-    ]
+  let halfA = a / 2
+      halfB = b / 2
+      halfC = c / 2
+      width = V3 a 0 0
+      height = V3 0 b 0
+      depth = V3 0 0 (-c)
+   in concatGeometries
+        [ tiledPlane (V3 (-halfA) (-halfB) halfC) width height 1 1,
+          tiledPlane (V3 halfA (-halfB) halfC) depth height 1 1,
+          tiledPlane (V3 halfA (-halfB) (-halfC)) (negated width) height 1 1,
+          tiledPlane (V3 (-halfA) (-halfB) (-halfC)) (negated depth) height 1 1,
+          tiledPlane (V3 (-halfA) halfB halfC) width depth 1 1,
+          tiledPlane (V3 (-halfA) (-halfB) (-halfC)) width (negated depth) 1 1
+        ]
